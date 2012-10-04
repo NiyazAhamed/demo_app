71e02b90a739cc84271f0962882e17aef5a26af3
Ahmed, Niyaz - CW <Niyaz.Ahmed@bestbuy.com>
2012-10-04 16:22:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5e3e100563ab3327043d7576d123105e4fae9ae7ed142527e7c4784b51423066b1b6f9e80605fc37344ea73d0b12cde4a82b628c81809f1826889e7d4d9487b'
